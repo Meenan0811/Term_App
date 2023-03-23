f0c3f530640f9a02d857e7ff87dcfc2ddf622330
Meenan0811 <meenan@mac.com>
2023-03-23 10:40:45 -0400

    Added Term Activites, constructors and getters/setters for entitites

---
+++ app/src/main/java/com/Meenan/Term_App/UI/CourseAdapter.java
@@ -0,0 +1,20 @@
+package com.Meenan.Term_App.UI;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class CourseAdapter extends RecyclerView<CourseAdapter.CourseViewHolder> {
+
+    public CourseAdapter(@NonNull Context context) {
+        super(context);
+    }
+
+
+    class CourseViewHolder extends RecyclerView.ViewHolder {
+
+    }
+}
