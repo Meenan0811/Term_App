77fe955c59e1c121985db275128c0645d7771c6d
Meenan0811 <meenan@mac.com>
2023-03-23 10:56:08 -0400

    Added Term Activites, constructors and getters/setters for entitites

--- app/src/main/java/com/Meenan/Term_App/Database/Repository.java
+++ app/src/main/java/com/Meenan/Term_App/Database/Repository.java
@@ -1,4 +1,7 @@
 package com.Meenan.Term_App.Database;
 
+import androidx.room.Database;
+
+
 public class Repository {
 }
---
+++ app/src/main/java/com/Meenan/Term_App/Database/TermCourseDataBase.java
@@ -0,0 +1,34 @@
+package com.Meenan.Term_App.Database;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+import com.Meenan.Term_App.DAO.CourseDAO;
+import com.Meenan.Term_App.DAO.TermDAO;
+import com.Meenan.Term_App.Entities.Course;
+import com.Meenan.Term_App.Entities.Term;
+
+@Database(entities = {Course.class, Term.class}, version = 1, exportSchema = false)
+public abstract class TermCourseDataBase extends RoomDatabase {
+
+    public abstract CourseDAO();
+    public abstract TermDAO();
+
+    public static volatile TermCourseDataBase INSTANCE;
+
+    static TermCourseDataBase getDataBase(final Context context) {
+        if (INSTANCE == null) {
+            synchronized (TermCourseDataBase.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(), TermCourseDataBase.class, "Term_Course.db")
+                            .fallbackToDestructiveMigration()
+                            .build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+}
