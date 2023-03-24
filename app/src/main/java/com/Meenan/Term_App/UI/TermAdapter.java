c461852aaade8763084fec55a6bfc3318fd36daa
Meenan0811 <meenan@mac.com>
2023-03-24 14:51:15 -0400

    Began coding TermAdapter and Term RecyclerView, added Repository

--- app/src/main/java/com/Meenan/Term_App/UI/TermAdapter.java
+++ app/src/main/java/com/Meenan/Term_App/UI/TermAdapter.java
@@ -1,4 +1,70 @@
 package com.Meenan.Term_App.UI;
 
-public class TermAdapter {
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.Meenan.Term_App.Entities.Term;
+import com.Meenan.Term_App.R;
+
+import java.util.List;
+
+public class TermAdapter extends RecyclerView.Adapter<TermAdapter.TermViewHolder> {
+
+    private List<Term> mTerms;
+    private Context context;
+
+    class TermViewHolder extends RecyclerView.ViewHolder {
+
+        private final TextView termItemView;
+
+        private TermViewHolder(View itemView) {
+            super(itemView);
+            termItemView = itemView.findViewById(R.id.termlist);
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    int position = getAdapterPosition();
+                    final Term curTerm = mTerms.get(position);
+                    Intent intent = new Intent(context, TermDetails.class);
+                    intent.putExtra("id", curTerm.getTermID());
+                    intent.putExtra("name", curTerm.getTermName());
+                    intent.putExtra("start", curTerm.getStartDate());
+                    intent.putExtra("end", curTerm.getEndDate());
+                    context.startActivity(intent);
+                }
+            });
+
+        }
+
+    }
+
+    private LayoutInflater mInflater;
+
+    public TermAdapter(Context context) {
+        mInflater = LayoutInflater.from(context);
+        this.context = context;
+    }
+    @NonNull
+    @Override
+    public TermAdapter.TermViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = mInflater.inflate(R.layout.term_list_item, parent, false);
+        return new TermViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull TermAdapter.TermViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
 }
